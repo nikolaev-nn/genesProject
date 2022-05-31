612471644164dcbadce110bfd17c6b4a2cb72a01
Yaroslav <jaroslav-nikiti@rambler.ru>
2022-05-31 20:38:47 +0500

    Update settings.py to deploy

--- gene_analysis/gene_analysis/settings.py
+++ gene_analysis/gene_analysis/settings.py
@@ -13,20 +13,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--#r55iu5hm0y-_(h8njb0*qokx)v$5l)cv%ulhrlqi-hzma+bl'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vm1789299.firstbyte.club', '185.255.134.98']
 
 
 # Application definition
@@ -82,7 +91,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'gene',
        'USER': 'postgres',
-       'PASSWORD': '2002',
+       'PASSWORD': env('POSTGRESQL_PASSWORD'),
        'HOST': 'localhost',
        'PORT': '5432',
     }
@@ -113,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -127,7 +136,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static/'), ]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static/'), ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'main/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
