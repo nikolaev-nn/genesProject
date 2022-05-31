649eacc5bb8e4689126dbc1e60fd1bebb71e4562
Yaroslav <jaroslav-nikiti@rambler.ru>
2022-06-01 02:33:40 +0500

    Add configs to deploy

--- gene_analysis/gene_analysis/settings.py
+++ gene_analysis/gene_analysis/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'gene',
-       'USER': 'postgres',
+       'USER': 'django',
        'PASSWORD': env('POSTGRESQL_PASSWORD'),
        'HOST': 'localhost',
        'PORT': '5432',
